d084ae451287479e1e378d7bff208e710a9e94c2
scriptkid777 <servsystem@yandex.ru>
2025-04-25 00:12:18 +0700

    1. Подключение к базе в веб приложении. Хранение вакансий. [#504859]

--- src/main/java/ru/job4j/dreamjob/service/SimpleFileService.java
+++ src/main/java/ru/job4j/dreamjob/service/SimpleFileService.java
@@ -19,8 +19,8 @@ public class SimpleFileService implements FileService {
 
     private final String storageDirectory;
 
-    public SimpleFileService(FileRepository fileRepository, @Value("${file.directory") String storageDirectory) {
-        this.fileRepository = fileRepository;
+    public SimpleFileService(FileRepository sql2oFileRepository, @Value("${file.directory") String storageDirectory) {
+        this.fileRepository = sql2oFileRepository;
         this.storageDirectory = storageDirectory;
         createStorageDirectory(storageDirectory);
     }
