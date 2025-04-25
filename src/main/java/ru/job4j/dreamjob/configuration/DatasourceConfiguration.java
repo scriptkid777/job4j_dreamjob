47d8f723f9693b28d1106187913d14d7e3e16b2f
scriptkid777 <servsystem@yandex.ru>
2025-04-26 00:03:42 +0700

    1. Подключение к базе в веб приложении. Хранение вакансий. [#504859]

--- src/main/java/ru/job4j/dreamjob/configuration/DatasourceConfiguration.java
+++ src/main/java/ru/job4j/dreamjob/configuration/DatasourceConfiguration.java
@@ -3,6 +3,7 @@ package ru.job4j.dreamjob.configuration;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.sql2o.Sql2o;
 import org.sql2o.converters.Converter;
 import org.sql2o.converters.ConverterException;
@@ -12,10 +13,10 @@ import org.sql2o.quirks.Quirks;
 import javax.sql.DataSource;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-
+@Configuration
 public class DatasourceConfiguration {
     @Bean
-    public DataSource connectionPool(@Value("${datasource.url") String url,
+    public DataSource connectionPool(@Value("${datasource.url}") String url,
                                      @Value("${datasource.username}") String username,
                                      @Value("${datasource.password}") String password) {
     return new BasicDataSource() {
