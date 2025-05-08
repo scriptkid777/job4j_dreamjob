adc6f893df0196f7efcb250898872ca5b164495e
scriptkid777 <servsystem@yandex.ru>
2025-05-09 01:52:15 +0700

    исправление

---
+++ src/test/java/Sql2oVacancyRepositoryTest.java
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import ru.job4j.dreamjob.configuration.DatasourceConfiguration;
+import ru.job4j.dreamjob.model.File;
+import ru.job4j.dreamjob.model.Vacancy;
+
+import ru.job4j.dreamjob.repository.Sql2oFileRepository;
+import ru.job4j.dreamjob.repository.Sql2oVacancyRepository;
+
+import java.util.Properties;
+
+import static java.time.LocalDateTime.now;
+import java.time.temporal.ChronoUnit;;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+public class Sql2oVacancyRepositoryTest {
+    private static Sql2oVacancyRepository sql2oVacancyRepository;
+
+    private static Sql2oFileRepository sql2oFileRepository;
+
+    private static File file;
+
+    @BeforeAll
+    public static void initRepositories() throws Exception {
+        var properties = new Properties();
+        try (var inputStream = Sql2oVacancyRepositoryTest.class.getClassLoader().getResourceAsStream("connection.properties")) {
+            properties.load(inputStream);
+        }
+        var url = properties.getProperty("datasource.url");
+        var username = properties.getProperty("datasource.username");
+        var password = properties.getProperty("datasource.password");
+        var configuration = new DatasourceConfiguration();
+        var datasource = configuration.connectionPool(url, username, password);
+        var sql2o = configuration.databaseClient(datasource);
+
+        sql2oVacancyRepository = new Sql2oVacancyRepository(sql2o);
+        sql2oFileRepository = new Sql2oFileRepository(sql2o);
+        // нужно сохранить хотя бы один файл, т.к. Vacancy от него зависит
+        file = new File("test", "test");
+        sql2oFileRepository.save(file);
+    }
+
+    @AfterAll
+    public static void deleteFile() {
+        sql2oFileRepository.deleteById(file.getId());
+    }
+
+    @AfterEach
+    public void clearVacancies() {
+        var vacancies = sql2oVacancyRepository.findAll();
+        for (var vacancy : vacancies) {
+            sql2oVacancyRepository.deleteById(vacancy.getId());
+        }
+    }
+
+    @Test
+    public void whenSaveThenGetSame() {
+        var creationDate = now().truncatedTo(ChronoUnit.MINUTES);
+        var vacancy = sql2oVacancyRepository.save(new Vacancy(0, "title", "description", creationDate, true, 1, file.getId()));
+        var savedVacancy = sql2oVacancyRepository.findById(vacancy.getId()).get();
+        assertThat(savedVacancy).usingRecursiveComparison().isEqualTo(vacancy);
+    }
+}
